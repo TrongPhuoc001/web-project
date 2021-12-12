ef3f5e47b6255d7c89b76f6fd1b62246eba2f145
Ngoc Nguyen <ngocnguyenc3@gmail.com>
2021-12-12 16:27:31 +0700

    Pagination table admin

--- thewayshop-admin/component/table/controller.js
+++ thewayshop-admin/component/table/controller.js
@@ -5,16 +5,24 @@ const view = '../component/table/view/';
 exports.viewTable = async function(req, res) {
 
   const {tb_name} = req.params;
+  const page = parseInt(req.query.page)||1;
   const column_name = await service.getColumnName(tb_name);
-  const record = await service.getAllRecord(tb_name) 
-
+  const record = await service.getRecord(page, tb_name);
+  console.log(page);
+  let max_page = await service.maxPage(tb_name);
+  max_page = parseInt(max_page.rows[0].max_page);
   return res.render(view+'table',{
     title:`Data for table ${tb_name}`,
     table_name:tb_name,
     column_name:column_name.rows,
     record: record.rows,
-    table_active:true
+    table_active:true,
+    page:page,
+    next:page<max_page?page+1 : false,
+    pages:Array.from({length: max_page}, (v, k) => k+1),
+    previous:page>1?page-1:false
   })
+
 };
 
 exports.editTable = async (req,res)=>{
--- thewayshop-admin/component/table/service.js
+++ thewayshop-admin/component/table/service.js
@@ -1,5 +1,5 @@
 const pool = require('../../models/config/dbconnect');
-
+const limit = 9;
 exports.getColumnName= (tb_name)=>{
     return pool.query(
         `SELECT column_name FROM information_schema.columns
@@ -18,4 +18,16 @@ exports.recordData = (tb_name,record_id)=>{
     return pool.query(
         `SELECT * FROM   ${tb_name}  WHERE id =$1;`,[record_id]
     )
+}
+exports.maxPage = (tb_name)=> {
+    return pool.query(
+        `SELECT ceil(COUNT(*)/$1::numeric) as max_page FROM ${tb_name};`,[limit]
+    )
+}
+exports.getRecord = (page, tb_name)=>{
+    return pool.query(
+        `SELECT * FROM ${tb_name}
+        ORDER BY id ASC
+        LIMIT $1 OFFSET $2;`,[limit,(page-1)*limit]
+      )
 }
