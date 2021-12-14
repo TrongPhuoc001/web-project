6cef46cac367bff7aff30809c9066bc75dc033a4
Ngoc Nguyen <ngocnguyenc3@gmail.com>
2021-12-14 10:23:50 +0700

    View info user in thewayshop-admin

--- thewayshop-admin/component/users/service.js
+++ thewayshop-admin/component/users/service.js
@@ -20,3 +20,11 @@ exports.block = (id, is_block) => {
         `UPDATE users SET is_delete = $2 WHERE id = $1;`[id, is_block]
     )
 }
+
+exports.recordData = (id) => {
+    return pool.query(
+        `SELECT users.id, users.email, users.name, users.address,users.balance,count(orders.id) as "Total orders",sum(orders.total) as "Expended", users.is_delete
+        FROM users left join orders on users.email = orders.email where users.id = $1
+        group by users.id;`,[id]
+    )
+}
