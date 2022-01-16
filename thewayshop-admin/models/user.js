25464a690cfbb69c996599c6ecebd7fc7fde8add
Ngoc Nguyen <ngocnguyenc3@gmail.com>
2022-01-16 17:48:08 +0700

    Fix top user total

--- thewayshop-admin/models/user.js
+++ thewayshop-admin/models/user.js
@@ -4,5 +4,6 @@ exports.getTotalUserRecord = pool.query(
     `select users.id, users.name, users.birthday, users.image, sum(orders.total) as total, count(orders.total) as total1
     from users, orders
     where users.id = orders.user_id 
-    group by users.id, users.name, users.birthday, users.image`
+    group by users.id, users.name, users.birthday, users.image
+    order by total desc`
 )
