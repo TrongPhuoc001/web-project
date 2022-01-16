63a2e9948302606c1a356a804ee6ca27c28ce804
trongphuoc001 <85543424+TrongPhuoc001@users.noreply.github.com>
2022-01-16 16:07:23 +0700

    clear shop page cache from admin

--- thewayshop/component/home/service.js
+++ thewayshop/component/home/service.js
@@ -2,20 +2,24 @@ const pool = require('../../models/dbconnect/dbconnect')
 
 
 const limit = 8;
-exports.getRecent = pool.query(
-    `SELECT id,title,price,image,state FROM product
-    WHERE is_delete = 'f'
-    AND state ='new'
-    ORDER BY create_date DESC
-    LIMIT $1;`,[limit]
-)
-exports.getTopSelling = pool.query(
-    `SELECT id,title,price,image,state FROM product
-    WHERE is_delete = 'f'
-    AND state='top'
-    ORDER BY sold DESC
-    LIMIT $1;`,[limit]
-)
+exports.getRecent = ()=>{
+    return pool.query(
+        `SELECT id,title,price,image,state FROM product
+        WHERE is_delete = 'f'
+        AND state ='new'
+        ORDER BY create_date DESC
+        LIMIT $1;`,[limit]
+    )
+} 
+exports.getTopSelling = ()=>{
+    return pool.query(
+        `SELECT id,title,price,image,state FROM product
+        WHERE is_delete = 'f'
+        AND state='top'
+        ORDER BY sold DESC
+        LIMIT $1;`,[limit]
+    )
+} 
 exports.searchProduct = (q,tag,brand,priceLow,priceHigh,page)=>{
     return pool.query(
         `SELECT product.id,title,description,price,image,state FROM product,tag
