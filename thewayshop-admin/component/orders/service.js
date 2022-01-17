0eb29c9b846539a932df3fdc9d39eb1749698ffd
trongphuoc001 <85543424+TrongPhuoc001@users.noreply.github.com>
2022-01-17 10:35:35 +0700

    finish v2.1

--- thewayshop-admin/component/orders/service.js
+++ thewayshop-admin/component/orders/service.js
@@ -7,7 +7,7 @@ exports.maxPage = ()=> {
 }
 exports.getAllRecord = (page)=>{
     return pool.query(
-        `SELECT orders.id,user_id,fullname,email,address,detail_address,payment,total,state as state_id,order_state.description as state,TO_CHAR(create_date::date, 'dd-mm-yyyy') as create_date,cancel FROM orders,order_state WHERE state=order_state.id
+        `SELECT orders.id,user_id,fullname,email,address,detail_address,payment,total,state as state_id,order_state.description as state,TO_CHAR(create_date::date, 'dd-mm-yyyy') as createdate,cancel FROM orders,order_state WHERE state=order_state.id ORDER BY create_date DESC
         LIMIT $1 OFFSET $2;`,[limit,(page-1)*limit]
     )
 }
